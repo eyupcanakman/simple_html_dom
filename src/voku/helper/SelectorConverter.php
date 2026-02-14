ee96621bdf61ba296f798f0e0d9167eb4bcb7bcb
Eyüp Can Akman <eyupcanakman@gmail.com>
2026-02-14 16:27:15 +0300

    [+]: handle leading combinator selectors (>, +, ~) in SelectorConverter
    Selectors like "> span" now work when called from element->find(),
    fixing issue #102. The combinator is intercepted before reaching
    Symfony's CssSelector and converted to the correct XPath axis.

--- src/voku/helper/SelectorConverter.php
+++ src/voku/helper/SelectorConverter.php
@@ -46,6 +46,37 @@ class SelectorConverter
             return $selector;
         }
 
+        // Handle selectors starting with a combinator (> child, + adjacent, ~ sibling)
+        $trimmedSelector = \ltrim($selector);
+        if (isset($trimmedSelector[0]) && \in_array($trimmedSelector[0], ['>', '+', '~'], true)) {
+            $combinator = $trimmedSelector[0];
+            $restSelector = \ltrim(\substr($trimmedSelector, 1));
+
+            if ($restSelector === '') {
+                throw new \RuntimeException('Selector cannot end with a combinator: ' . $selector);
+            }
+
+            $restXPath = self::toXPath($restSelector, $ignoreCssSelectorErrors, $isForHtml);
+
+            switch ($combinator) {
+                case '>':
+                    $xPathQuery = \str_replace('descendant-or-self::', '/*/', $restXPath);
+                    break;
+                case '+':
+                    $xPathQuery = \str_replace('descendant-or-self::', '/*/following-sibling::*[1]/self::', $restXPath);
+                    break;
+                case '~':
+                    $xPathQuery = \str_replace('descendant-or-self::', '/*/following-sibling::', $restXPath);
+                    break;
+                default:
+                    throw new \RuntimeException('Unexpected combinator: ' . $combinator);
+            }
+
+            self::$compiled[$selector] = $xPathQuery;
+
+            return $xPathQuery;
+        }
+
         if (!\class_exists(CssSelectorConverter::class)) {
             throw new \RuntimeException('Unable to filter with a CSS selector as the Symfony CssSelector 2.8+ is not installed (you can use filterXPath instead).');
         }
